6a7fa3357ab76d311dbc08e69a9f72ba84d4872f
Anthony <anthony.reynaud@hetic.net>
2017-12-19 14:41:07 +0100

    béta

--- js/script.js
+++ js/script.js
@@ -2,9 +2,22 @@
 //{
   var player = document.querySelector('.player');
   var catbutt = document.querySelector('.catbutt');
+  var arrow = document.querySelector('.arrow');
   var categories = document.querySelector('.categories');
   var cat_list = document.querySelectorAll(".cat");
   var movies = document.querySelector('.movies');
+  var film = document.querySelectorAll('.film');
+  var loadmore = document.querySelector('.loadmore');
+
+  catbutt.addEventListener('click', function() {
+    categories.classList.toggle('open');
+    arrow.classList.toggle('rotate');
+  });
+
+  arrow.addEventListener('click', function() {
+    categories.classList.toggle('open');
+    arrow.classList.toggle('rotate');
+  });
 
   load_films("See All");
 
@@ -14,12 +27,12 @@
     return str;
   }
 
-  function add_film(film)
+  function add_film(newfilm)
   {
     movies.innerHTML +=
-    "<article class=\"film\">" +
-    "<h2 class=\"film_title\">" + film.title + "</h2>" +
-    "<p class=\"film_description\">" + shorten_text(film.description, 40) + "</p>";
+    "<article class=\"film\" style=\"background:url("+newfilm.image_url+");\">" +
+    "<h2 class=\"film_title\">" + newfilm.title + "</h2>" +
+    "<p class=\"film_description\">" + shorten_text(newfilm.description, 40) + "</p>";
   }
 
   function load_films(category)
@@ -42,9 +55,7 @@
     }
   }
 
-  catbutt.addEventListener('click', function() {
-    categories.classList.toggle('open');
-  });
+
 
   for (let i = 0; i < cat_list.length; i++)
   {
@@ -54,15 +65,38 @@
     });
   }
 
-  /*film.addEventListener('mouseover', function() {
-    film.classList.add('syno');
+  loadmore.addEventListener('click', function(){
+    movies.style.height = '100%';
+    loadmore.style.display = 'none';
   });
 
-  film.addEventListener('mouseout', function() {
-    film.classList.remove('syno');
-  });*/
+  document.addEventListener('DOMContentLoaded', function() {
+    var burger = document.querySelector('.burger');
+    var bar0 = document.querySelector('.bar0');
+    var bar1 = document.querySelector('.bar1');
+    var bar2 = document.querySelector('.bar2');
+    var nav = document.querySelector('.nav');
+    var body = document.querySelector('.body');
 
-  // film.addEventListener('click', function() {
-  //   player.classList.toggle('block');
-  // });
-//});
+    burger.addEventListener('click', function() {
+      nav.classList.toggle('on');
+      body.style.overflow = 'hidden';
+      bar0.classList.toggle('cross0');
+      bar1.classList.toggle('cross1');
+      bar2.classList.toggle('cross2');
+    });
+  });
+
+  // for (let i = 0; i < film.length; i++) {
+  //   film[i].addEventListener('mouseover', function() {
+  //     film[i].classList.add('syno');
+  //   });
+  //
+  //   film.addEventListener('mouseout', function() {
+  //     film.classList.remove('syno');
+  //   });
+  //
+  //   film[i].addEventListener('click', function() {
+  //     player.classList.toggle('block');
+  //   });
+  // }
